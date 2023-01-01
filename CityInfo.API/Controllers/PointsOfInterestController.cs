c4d2f46a3797846798195d9a86a9ddd238d7c672
jsureka <bsse1115@iit.du.ac.bd>
2023-01-01 19:08:36 +0600

    Files controller added

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -8,6 +8,32 @@ namespace CityInfo.API.Controllers
     [ApiController]
     public class PointsOfInterestController : ControllerBase
     {
-        public ActionResult<IEnumerable<PointOfInterestDto>>
+        [HttpGet]
+        public ActionResult<IEnumerable<PointOfInterestDto>> GetPointOfInterest(int cityId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if( city is null)
+            {
+                return NotFound();
+            }
+            return Ok(city.PointOfInterests);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<PointOfInterestDto> GetSinglePointOfInterest(int cityId, int id)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city is null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterest = city.PointOfInterests.FirstOrDefault(c => c.Id == id);
+            if(pointOfInterest is null)
+            {
+                return NotFound();
+            }
+            return Ok(pointOfInterest);
+        }
     }
 }
