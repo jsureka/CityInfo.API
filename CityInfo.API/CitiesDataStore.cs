a887f150b6c72c79eab8f25796a80a62f649a341
jsureka <bsse1115@iit.du.ac.bd>
2023-01-01 15:34:49 +0600

    Add project files.

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,29 @@
+﻿using CityInfo.API.Models;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public List<CityDto> Cities { get; set; }
+        public static CitiesDataStore Current { get; set; } = new CitiesDataStore();
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "New York City",
+                    Description = "The one with statue of liberty"
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Antwerp",
+                    Description = "This is antwerp city"
+                }
+            };
+        }
+    }
+}
